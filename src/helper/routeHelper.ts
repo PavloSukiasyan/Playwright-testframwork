7c4c49f51add071647bc5621d8f6879e3d5ca083
Pavlo Sukiasyan <pavlo.sukiasyan@gmail.com>
2023-01-27 16:51:33 +0200

    Added test with Mock, changed structure, some esLint changes.

---
+++ src/helper/routeHelper.ts
@@ -0,0 +1,33 @@
+import { Page } from '@playwright/test';
+
+function isErrorResponse(mock: unknown) {
+  return typeof mock === 'object' && mock !== null && 'errors' in mock;
+}
+
+function isWhoosmappResponse(mock: unknown) {
+  return typeof mock === 'object' && mock !== null && 'localities' in mock;
+}
+
+export const routeHelper = async (
+  page: Page,
+  url: string,
+  contentToMock: unknown,
+) => {
+  await page.route(`**/${url}`, (route) => {
+    const response = isErrorResponse(contentToMock) || isWhoosmappResponse(contentToMock)
+      ? contentToMock
+      : { data: contentToMock };
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    route.fulfill({
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      status: 200,
+      contentType: 'json',
+      body: JSON.stringify(response),
+    });
+  });
+};
+
+export default routeHelper;

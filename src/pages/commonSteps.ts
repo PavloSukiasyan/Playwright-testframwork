1bb9618e7bc220d84eed48a9a2858d7495a4ea1b
Pavlo Sukiasyan <pavlo.sukiasyan@gmail.com>
2023-02-14 12:33:26 +0200

    PT-8: test structure and grantGeolocationPrermission

--- src/pages/commonSteps.ts
+++ src/pages/commonSteps.ts
@@ -1,10 +1,15 @@
-import { Page } from '@playwright/test';
+import { BrowserContext, Page } from '@playwright/test';
 
 export class CommonSteps {
-  constructor(private readonly page: Page, private readonly tInfo: string = '') {}
+  constructor(private readonly page: Page, private context: BrowserContext) {}
 
   async goToHomePage() {
+    await this.grantGeoPermissions();
     await this.page.goto('https://www.bathrooms.com/');
+  }
+
+  async grantGeoPermissions() {
+    await this.context.grantPermissions(['geolocation']);
   }
 }
 
--- src/tests/ui_functional/staticPages/cookiesPolicy.spec.ts
+++ src/tests/ui_functional/staticPages/cookiesPolicy.spec.ts
@@ -6,34 +6,45 @@ import Footer from '../../../pages/footer';
 import StaticPage from '../../../pages/staticPages/staticPage';
 import { mockForCookiesPolicy } from './mockCookiesPolicy';
 
-test.use({
-  permissions: ['geolocation'],
-});
+test.describe('Tests for Static Cookies policy page: ', () => {
+  test.beforeEach(async ({ page, context }) => {
+    const commonSteps = new CommonSteps(page, context);
+    const footer = new Footer(page);
+    const cookiesPolicy = new CookiesPolicyComponent(page, context);
 
-test('PT-1, open Cookie Policy page', async ({ page, context }) => {
-  const commonSteps = new CommonSteps(page);
-  const staticPage = new StaticPage(page);
-  const footer = new Footer(page);
-  const cookiesPolicy = new CookiesPolicyComponent(page, context);
+    await cookiesPolicy.setPredefinedCookies();
+    await commonSteps.goToHomePage();
 
-  await cookiesPolicy.setPredefinedCookies();
-  await commonSteps.goToHomePage();
+    // Mock
+    await routeHelper(page, 'content/v1/spaces/g44e4oo0e2sa/environments/master', mockForCookiesPolicy);
 
-  await expect.soft(page).toHaveTitle(
-    'The Bathroom Showroom | 280+ Showrooms nationwide',
-  );
+    // click footer link
+    await footer.navigationPart.waitFor();
+    await footer.getFooterLinkByHref('cookies-policy').click();
+  });
 
-  // Mock
-  await routeHelper(page, 'content/v1/spaces/g44e4oo0e2sa/environments/master', mockForCookiesPolicy);
+  test('PT-1, open Cookie Policy page', async ({ page }) => {
+    const staticPage = new StaticPage(page);
 
-  // click footer link
-  await expect(footer.navigationPart).toBeVisible();
-  await footer.getFooterLinkByHref('cookies-policy').click();
+    await expect.soft(page).toHaveURL(
+      '/legal-policies/cookies-policy',
+    );
 
-  await expect.soft(page).toHaveTitle(
-    'The Bathroom Showroom | 280+ Showrooms nationwide',
-  );
+    await expect.soft(page).toHaveTitle(
+      'Cookies Policy | The Bathroom Showroom',
+    );
 
-  await expect.soft(staticPage.title).toHaveText('Cookies Policy Mock');
-  await expect.soft(staticPage.title).toHaveCSS('font-size', '36px');
+    await expect.soft(staticPage.title).toHaveText('Cookies Policy Mock');
+    await expect.soft(staticPage.title).toHaveCSS('font-size', '36px');
+    await expect.soft(staticPage.title).toHaveCSS('color', 'rgb(0, 0, 0)');
+  });
+
+  test('PT-2, page content and style', async ({ page }) => {
+    const staticPage = new StaticPage(page);
+
+    // TODO
+    await expect.soft(staticPage.title).toHaveText('Cookies Policy Mock');
+    await expect.soft(staticPage.title).toHaveCSS('font-size', '36px');
+    await expect.soft(staticPage.title).toHaveCSS('color', 'rgb(0, 0, 0)');
+  });
 });

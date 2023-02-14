f97d34909b53df2d1bdbe01e1337c7c8b149ba18
Pavlo Sukiasyan <pavlo.sukiasyan@gmail.com>
2023-02-14 16:03:38 +0200

    PT-8: added Bredcrumbs locators and updated tests

--- src/tests/ui_functional/staticPages/cookiesPolicy.spec.ts
+++ src/tests/ui_functional/staticPages/cookiesPolicy.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { routeHelper } from '../../../helper/routeHelper';
 import { CommonSteps } from '../../../pages/commonSteps';
+import BreadCrumbsComponent from '../../../pages/components/breadcrumbs';
 import CookiesPolicyComponent from '../../../pages/components/oneTrustCookiePolicy';
 import Footer from '../../../pages/footer';
 import StaticPage from '../../../pages/staticPages/staticPage';
@@ -23,8 +24,8 @@ test.describe('Tests for Static Cookies policy page: ', () => {
     await footer.getFooterLinkByHref('cookies-policy').click();
   });
 
-  test('PT-1, open Cookie Policy page', async ({ page }) => {
-    const staticPage = new StaticPage(page);
+  test('PT-1, open Cookie Policy page and Breadcrumbs', async ({ page }) => {
+    const breadcrumbs = new BreadCrumbsComponent(page);
 
     await expect.soft(page).toHaveURL(
       '/legal-policies/cookies-policy',
@@ -34,17 +35,42 @@ test.describe('Tests for Static Cookies policy page: ', () => {
       'Cookies Policy | The Bathroom Showroom',
     );
 
-    await expect.soft(staticPage.title).toHaveText('Cookies Policy Mock');
-    await expect.soft(staticPage.title).toHaveCSS('font-size', '36px');
-    await expect.soft(staticPage.title).toHaveCSS('color', 'rgb(0, 0, 0)');
+    await expect.soft(breadcrumbs.bigTitle).toHaveText('Cookies Policy Mock');
+    await expect.soft(breadcrumbs.bigTitle).toHaveCSS('font-size', '36px');
+    await expect.soft(breadcrumbs.bigTitle).toHaveCSS('color', 'rgb(0, 0, 0)');
+
+    const brItems = await (breadcrumbs.brLinks).count();
+
+    for (let index = 0; index < brItems; index += 1) {
+      await expect.soft(breadcrumbs.brLinks.nth(index)).toHaveCSS('font-size', '12px');
+      await expect.soft(breadcrumbs.brLinks.nth(index)).toHaveCSS('font-weight', '400');
+      await expect.soft(breadcrumbs.brLinks.nth(index)).toHaveCSS('text-align', 'left');
+      await expect.soft(breadcrumbs.brLinks.nth(index)).toHaveCSS('text-overflow', 'ellipsis');
+      await expect.soft(breadcrumbs.brLinks.nth(index)).toHaveCSS('-webkit-line-clamp', '2');
+    }
+
+    for (let i = 0; i < 1; i += 1) {
+      await expect.soft(breadcrumbs.brLinks.nth(i)).toHaveCSS('color', 'rgb(5, 45, 91)');
+      await expect.soft(breadcrumbs.brLinks.nth(i)).toHaveCSS('background-color', 'rgba(0, 0, 0, 0)');
+      await expect.soft(breadcrumbs.brLinks.nth(i)).toHaveCSS('cursor', 'pointer');
+    }
+
+    await expect.soft(breadcrumbs.brLinks.nth(2)).toHaveCSS('color', 'rgb(96, 111, 128)');
+    await expect.soft(breadcrumbs.brLinks.nth(2)).toHaveCSS('background-color', 'rgba(0, 0, 0, 0)');
+    await expect.soft(breadcrumbs.brLinks.nth(2)).toHaveCSS('cursor', 'default');
+
+    const brLabels = ['Homepage', 'Legal policies', 'Cookies Policy Mock'];
+    for (let i = 0; i < brLabels.length; i += 1) {
+      await expect.soft(breadcrumbs.brLinks.nth(i)).toHaveText(brLabels[i]);
+    }
   });
 
   test('PT-2, page content and style', async ({ page }) => {
     const staticPage = new StaticPage(page);
 
     // TODO
-    await expect.soft(staticPage.title).toHaveText('Cookies Policy Mock');
-    await expect.soft(staticPage.title).toHaveCSS('font-size', '36px');
+    await expect.soft(staticPage.title).toHaveText('Cookies Policy Mock2');
+    await expect.soft(staticPage.title).toHaveCSS('font-size', '24px');
     await expect.soft(staticPage.title).toHaveCSS('color', 'rgb(0, 0, 0)');
   });
 });

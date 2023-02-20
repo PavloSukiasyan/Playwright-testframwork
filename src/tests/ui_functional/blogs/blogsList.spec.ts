cdc6985e056d2de4bb686ba7fe5d8fa1071ccf1a
Pavlo Sukiasyan <pavlo.sukiasyan@gmail.com>
2023-02-20 14:25:17 +0200

    PT-12: Updated blog List mock, added tests BCOM-5.

--- src/tests/ui_functional/blogs/blogsList.spec.ts
+++ src/tests/ui_functional/blogs/blogsList.spec.ts
@@ -21,8 +21,7 @@ test.describe('Tests for Blogs listing page: ', () => {
     await commonSteps.goToHomePage();
 
     // Mock
-    // ToDo add more Blogs to mock, and make default
-    await routeHelper(page, 'content/v1/spaces/g44e4oo0e2sa/environments/master', mockForBlogsListing(0, 2));
+    await routeHelper(page, 'content/v1/spaces/g44e4oo0e2sa/environments/master', mockForBlogsListing());
 
     await footer.navigationPart.waitFor();
     await footer.getFooterLinkByHref('blogs').click();
@@ -38,6 +37,54 @@ test.describe('Tests for Blogs listing page: ', () => {
     );
 
     await expect.soft(breadcrumbs.bigTitle).toHaveText('Welcome to our blog');
-    await expect.soft(blogList.titleMainArticle).toHaveText('Mock Add Soft Curves With Circular Bathroom Mirrors');
+
+    await expect.soft(breadcrumbs.brLinks).toHaveCount(2);
+    await expect.soft(breadcrumbs.brLinks.nth(0)).toHaveText('Homepage');
+    await expect.soft(breadcrumbs.brLinks.nth(1)).toHaveText('Blog');
   });
+
+  test('BCOM-5, main article', async () => {
+    await expect.soft(blogList.titleMainArticle).toHaveText('Mock Add Soft Curves With Circular Bathroom Mirrors');
+    await expect.soft(blogList.titleMainArticle).toHaveCSS('font-size', '36px');
+    await expect.soft(blogList.titleMainArticle).toHaveCSS('font-weight', '700');
+    await expect.soft(blogList.titleMainArticle).toHaveCSS('font-family', /Helvetica Neue/);
+    await expect.soft(blogList.titleMainArticle).toHaveCSS('text-align', 'start');
+    await expect.soft(blogList.titleMainArticle).toHaveCSS('text-overflow', 'ellipsis');
+    await expect.soft(blogList.titleMainArticle).toHaveCSS('-webkit-line-clamp', '2');
+    await expect.soft(blogList.titleMainArticle).toHaveCSS('color', 'rgb(24, 45, 61)');
+
+    await expect.soft(blogList.descriptionMainArticle)
+      .toHaveText('A fantastic way of adding soft curves to your bathroom is through the use of stunning-looking circular mirrors.');
+    await expect.soft(blogList.descriptionMainArticle).toHaveCSS('font-size', '16px');
+    await expect.soft(blogList.descriptionMainArticle).toHaveCSS('font-weight', '400');
+    await expect.soft(blogList.descriptionMainArticle).toHaveCSS('font-family', /Helvetica Neue/);
+    await expect.soft(blogList.descriptionMainArticle).toHaveCSS('text-align', 'left');
+    await expect.soft(blogList.descriptionMainArticle).toHaveCSS('text-overflow', 'ellipsis');
+    await expect.soft(blogList.descriptionMainArticle).toHaveCSS('-webkit-line-clamp', '3');
+    await expect.soft(blogList.descriptionMainArticle).toHaveCSS('color', 'rgb(24, 45, 61)');
+
+    await expect.soft(blogList.dateOfMainArticle).toHaveText('8th Feb 2023');
+    await expect.soft(blogList.dateOfMainArticle).toHaveCSS('font-size', '14px');
+    await expect.soft(blogList.dateOfMainArticle).toHaveCSS('font-weight', '400');
+    await expect.soft(blogList.dateOfMainArticle).toHaveCSS('font-family', /Helvetica Neue/);
+    await expect.soft(blogList.dateOfMainArticle).toHaveCSS('text-align', 'left');
+    await expect.soft(blogList.dateOfMainArticle).toHaveCSS('text-overflow', 'clip');
+    await expect.soft(blogList.dateOfMainArticle).toHaveCSS('-webkit-line-clamp', 'none');
+    await expect.soft(blogList.dateOfMainArticle).toHaveCSS('color', 'rgb(96, 111, 128)');
+
+    await expect.soft(blogList.readingOfMainArticle).toHaveText('5 min');
+    await expect.soft(blogList.readingOfMainArticle).toHaveCSS('font-size', '14px');
+    await expect.soft(blogList.readingOfMainArticle).toHaveCSS('font-weight', '400');
+    await expect.soft(blogList.readingOfMainArticle).toHaveCSS('font-family', /Helvetica Neue/);
+    await expect.soft(blogList.readingOfMainArticle).toHaveCSS('text-align', 'left');
+    await expect.soft(blogList.readingOfMainArticle).toHaveCSS('text-overflow', 'clip');
+    await expect.soft(blogList.readingOfMainArticle).toHaveCSS('-webkit-line-clamp', 'none');
+    await expect.soft(blogList.readingOfMainArticle).toHaveCSS('color', 'rgb(96, 111, 128)');
+
+    await expect.soft(blogList.btnMainArticle).toHaveText('Read more');
+    await expect.soft(blogList.btnMainArticle).toHaveCSS('color', 'rgb(255, 255, 255)');
+    await expect.soft(blogList.btnMainArticle).toHaveCSS('background-color', 'rgb(60, 113, 188)');
+  });
+
+  // ToDo regular articles test,  tests with load more + filtering
 });

3bcdb462e72909569db702c7340f6b45e9778089
Pavlo Sukiasyan <pavlo.sukiasyan@gmail.com>
2025-03-10 09:37:57 +0200

    PT-45: Debug and other changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? [
     ['html', { open: 'never' }],
